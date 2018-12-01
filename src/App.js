e3065924edc1572c795d5e6fea2412240d8344e9
Dendi Handian <handian.dendi@gmail.com>
2018-12-01 18:13:11 +0700

    hoc example

--- src/App.js
+++ src/App.js
@@ -3,10 +3,29 @@ import React, { Component } from 'react';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-      </div>
+      <h1>
+        Hello
+      </h1>
     );
   }
 }
+
+function myTestWrapper(WrappedComponent) {
+  return class extends Component {
+    render() {
+      return (
+        <div
+          style={{
+            backgroundColor: "blue"
+          }}
+        >
+          <WrappedComponent />
+        </div>
+      )
+    }
+  }
+}
+
+App = myTestWrapper(App)
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import App from './App';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { LoggerExample } from './loggerExample'
+// import { LoggerExample } from './loggerExample'
 
-// ReactDOM.render(<App />, document.getElementById('root'));
-ReactDOM.render(<LoggerExample />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<LoggerExample />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
