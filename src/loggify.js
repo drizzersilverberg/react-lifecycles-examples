eb9a2b0561dfc1da5ca5f0b8d6fb34e4aaf3ea06
Dendi Handian <handian.dendi@gmail.com>
2018-12-01 23:19:37 +0700

    setState example

--- src/loggify.js
+++ src/loggify.js
@@ -24,6 +24,13 @@ export default function loggify(Wrapped) {
         original(...args)
       }
     }
+    Wrapped.prototype.setState = function (partialState, callback) {
+      console.groupCollapsed(`${Wrapped.displayName} setState`)
+      console.log('partialState', partialState)
+      console.log('callback', callback)
+      console.groupEnd()
+      this.updater.enqueueSetState(this, partialState, callback, 'setState')
+    }
   })
 
   return class extends Component {
